9c8f47a8296b8eb3734c473a1b8526a45597f9d8
Christopher Villamarín <alexpila.314@gmail.com>
2023-09-25 19:33:34 -0500

    fix: remove username by default in build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,3 @@ pip3 install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('xeland314', 'alexpila.314@gmail.com', 'indrod_group_pass')"
