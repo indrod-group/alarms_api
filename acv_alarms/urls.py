0c99c43e90dabc357a69e9b467c1b51d43fc2a2b
Christopher Villamarín <alexpila.314@gmail.com>
2023-10-26 11:37:57 -0500

    feat(urls): add route to generate token to authenticate

--- acv_alarms/urls.py
+++ acv_alarms/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 urlpatterns = [
+    path('api_generate_token/', views.obtain_auth_token),
     path('admin/', admin.site.urls),
     path('api/v1/', include('alarms.urls')),
 ]
